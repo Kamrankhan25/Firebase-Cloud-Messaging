aee3df4fa3b1bc31a4095938d66d908bfa872d1a
Kamrab <kamran@gmail.com>
2022-10-05 10:43:10 +0500

    FCM App

---
+++ app/src/main/java/com/fcm/firebase/messaging/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fcm.firebase.messaging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //https://github.com/Kamrankhan25/Firebase-Cloud-Messaging
+    }
+}
